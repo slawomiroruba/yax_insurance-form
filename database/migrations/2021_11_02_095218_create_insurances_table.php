5273b586b544e67906795612ef670272ed4f15c6
Sławomir Oruba <sawomir.oruba@gmail.com>
2021-11-02 11:05:46 +0100

    Initial commit

---
+++ database/migrations/2021_11_02_095218_create_insurances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInsurancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('insurances', function (Blueprint $table) {
+            $table->id();
+            $table->string('profession_number');
+            $table->string('email');
+            $table->string('insurance-type');
+            $table->string('archived');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('insurances');
+    }
+}
